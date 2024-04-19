1811504b1bf2414fabde67e2a4d4e2be11991c05
ZLY201 <951711127@qq.com>
2024-04-19 22:19:46 +0800

    feat: add request cache

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,7 +5,12 @@ const staticAssetServerUrlDefault = SERVER_URL;
 const staticAssetServerUrlForCN =
   'https://7072-prod-9goxmz5w39f71724-1308749526.tcb.qcloud.la/';
 
+let cache: string | undefined;
+
 export async function getStaticAssetServerUrl() {
+  if (cache) {
+    return cache;
+  }
   try {
     const res = await Promise.race<string>([
       fetch(`${staticAssetServerUrlDefault}res/studio.env`).then(
@@ -17,6 +22,7 @@ export async function getStaticAssetServerUrl() {
         }, 1000);
       }),
     ]);
+    cache = res;
     return res;
   } catch {
     return staticAssetServerUrlDefault;
