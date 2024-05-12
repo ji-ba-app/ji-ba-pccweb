a34f8ec1116e0e57087749d6f5aaff441b416f03
ZLY201 <951711127@qq.com>
2024-05-12 17:24:09 +0800

    feat: optimize cdn domain check

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,31 +5,10 @@ const staticAssetServerUrlDefault = SERVER_URL;
 const staticAssetServerUrlForCN =
   'https://7072-prod-9goxmz5w39f71724-1308749526.tcb.qcloud.la/';
 
-let cache: string | undefined;
-
 export async function getStaticAssetServerUrl() {
-  if (cache) {
-    return cache;
-  }
-  try {
-    const res = await Promise.race<string>([
-      fetch(`${staticAssetServerUrlDefault}res/studio.env`, {
-        cache: 'no-store',
-        headers: {
-          'Cache-Control': 'no-cache',
-        },
-      })
-        .then(res => res.text())
-        .then(() => staticAssetServerUrlDefault),
-      new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve(staticAssetServerUrlForCN);
-        }, 1000);
-      }),
-    ]);
-    cache = res;
-    return res;
-  } catch {
+  const domain = window.location.href;
+  if (domain.includes('github') || domain.includes('http://')) {
     return staticAssetServerUrlDefault;
   }
+  return staticAssetServerUrlForCN;
 }
